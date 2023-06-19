e0492c72deeaddef27f6995a3f764abf3ea10a53
Abu-Sameer <adesoyeabolajiyusuph@gmail.com>
2023-06-19 16:48:27 +0100

    Todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,5 @@ import React from 'react';
 import AdvTodoMain from './component/AdvTodoMain';
 
 export default function App() {
-  return (
-    <div>
-      <AdvTodoMain />
-    </div>
-  );
+  return <div>App</div>;
 }
